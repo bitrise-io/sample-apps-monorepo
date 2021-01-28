fbcc268bc1104307675217c697eaead73e04e9ff
DamienBitrise <damien.murphy@bitrise.io>
2021-01-28 09:07:00 -0800

    initial commit

---
+++ ios/NotesUITests/more tests/Notesx2Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+	class Notesx2Tests: XCTestCase {
+	     override func setUp() {
+	         super.setUp()
+	         XCUIApplication().launch()
+	     }
+	     override func tearDown() {
+	         super.tearDown()
+	     }
+	     func testExample() {
+	     }
+	 }
